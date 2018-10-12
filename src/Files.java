deae4604750ccfb73317a59cf153761311c49cf4
Liwiusz Piórko <liwiuszpiorko@wp.pl>
2018-10-12 13:15:16 +0200

    v2

--- src/Files.java
+++ src/Files.java
@@ -1,21 +1,15 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+
 public class Files {
-    public static void main(String[] args)throws IOException {
-        File plik=new File("C:\\Użytkownicy\\user\\Pulpit\\produkty.txt");
-        Scanner plikSkan=new Scanner(plik);
-        System.out.println(plikSkan.nextLine());
-        System.out.println(plikSkan.hasNextLine());
-        System.out.println(plikSkan.nextInt());
-        System.out.println(plikSkan.nextLine());
-        System.out.println(plikSkan.nextLine());
-        System.out.println(plikSkan.nextInt());
-
-
+    public static void main(String[] args) throws IOException {
+        //Products[]productsArray=new Products[6];
+        File file = new File("produkty.txt");
+        Scanner fileScan = new Scanner(file);
+        while (fileScan.hasNext()) {
+            String products = fileScan.next();
+            System.out.println(products);
+        }
     }
-
-
-
-
 }
---
+++ src/Products.java
@@ -0,0 +1,35 @@
+public class Products {
+    private String name;
+    private String manufacturer;
+    private double price;
+
+    public Products(String name, String manufacturer, double price) {
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
