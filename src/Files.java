a462ddea43435f8926f2413bf6cb66b28293ec19
Liwiusz Piórko <liwiuszpiorko@wp.pl>
2018-10-17 11:50:32 +0200

    v3

--- src/Files.java
+++ src/Files.java
@@ -1,15 +1,22 @@
 import java.io.File;
 import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
+
 public class Files {
-    public static void main(String[] args) throws IOException {
-        //Products[]productsArray=new Products[6];
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+
         File file = new File("produkty.txt");
-        Scanner fileScan = new Scanner(file);
-        while (fileScan.hasNext()) {
-            String products = fileScan.next();
-            System.out.println(products);
+        try (Scanner fileScan = new Scanner(file)) {
+            while (fileScan.hasNext()) {
+                String nextLine = fileScan.next();
+                String[] product = nextLine.split(" ");
+                System.out.println(Arrays.toString(product));
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Plik nie istnieje");
         }
     }
 }
--- src/Products.java
+++ src/Products.java
@@ -3,7 +3,9 @@ public class Products {
     private String manufacturer;
     private double price;
 
-    public Products(String name, String manufacturer, double price) {
+    public Products(){}
+
+   public Products(String name, String manufacturer, double price) {
         this.name = name;
         this.manufacturer = manufacturer;
         this.price = price;
@@ -32,4 +34,23 @@ public class Products {
     public void setPrice(double price) {
         this.price = price;
     }
-}
+
+    @Override
+    public String toString() {
+        return "Products{" +
+                "name='" + name + '\'' +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+
+    //  public double returnSum(double price){
+    //    return;
+
+
+    //}
+   // public void showMostExpensive(){
+
+    }
+//}
