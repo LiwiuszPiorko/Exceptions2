f0acfee4eadbad33c719e8b2865410f8ec503f16
Liwiusz Piórko <liwiuszpiorko@wp.pl>
2018-10-18 13:42:37 +0200

    v4

--- src/Files.java
+++ src/Files.java
@@ -8,12 +8,15 @@ import java.util.Scanner;
 public class Files {
     public static void main(String[] args) throws FileNotFoundException, IOException {
 
+        String[] products1 = new String[1];
         File file = new File("produkty.txt");
         try (Scanner fileScan = new Scanner(file)) {
+
             while (fileScan.hasNext()) {
-                String nextLine = fileScan.next();
-                String[] product = nextLine.split(" ");
-                System.out.println(Arrays.toString(product));
+                for (int i = 0; i < products1.length; i++) {
+                    products1[i] = fileScan.next();
+                }
+                System.out.print(Arrays.toString(products1));
             }
         } catch (FileNotFoundException e) {
             System.err.println("Plik nie istnieje");
